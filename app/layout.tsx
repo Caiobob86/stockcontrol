25552a0abee813eeeb29350147e4ae68f170b671
Caiobob86 <ccleme1986@gmail.com>
2026-07-28 15:24:56 -0300

    Adiciona layout principal do aplicativo

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "StockControl",
+  description: "Sistema de controle de materiais",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
